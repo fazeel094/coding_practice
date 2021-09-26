420b2cf2d68a9b695ccc41cba0032367641a3e31
fazeel <fazeelfz6@gmail.com>
2021-09-26 17:42:03 +0530

    4. Boilerplate code for C

---
+++ chapter 1/4_Boilerplatecode.c
@@ -0,0 +1,25 @@
+//{
+	// Place your snippets for c here. Each snippet is defined under a snippet name and has a prefix, body and 
+	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the 
+	// same ids are connected.
+	// Example:
+// 	"Print to console": {
+// 		"prefix": "C boilerplate code",
+// 		"body": [
+// 			"#include<stdio.h>\n",
+// 			"int main(){",
+// 			"\t$1",
+// 			"\treturn 0;",
+// 			"}"
+// 		],
+// 		"description": "This is my boilerplate code for running 'C'"
+// 	}
+// }
+
+#include<stdio.h>
+
+int main(){
+    printf("I have used user snippets to trigger the prefix");
+    return 0;
+}
